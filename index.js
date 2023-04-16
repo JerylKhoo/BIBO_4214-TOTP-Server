933557c157b9e2fe67ad5071e4b3c9c068071a31
acidMyke <13977709+acidMyke@users.noreply.github.com>
2023-04-16 18:11:11 +0800

    remove unused json-db

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const JsonDB = require('node-json-db').JsonDB;
-const Config = require('node-json-db/dist/lib/JsonDBConfig').Config;
 const uuid = require('uuid');
 const speakeasy = require('speakeasy');
 const cors = require('cors');
@@ -26,21 +24,6 @@ if (QR_KEY === undefined) {
 
 const app = express();
 app.use(cors());
-/**
- * Creates a node-json-db database config
- * @param {string} name - name of the JSON storage file
- * @param {boolean} Tells the to save on each push otherwise the save() mthod has to be called.
- * @param {boolean} Instructs JsonDB to save the database in human readable format
- * @param {string} separator - the separator to use when accessing database values
- */
-const dbConfig = new Config('myDataBase', true, false, '/');
-
-/**
- * Creates a Node-json-db JSON storage file
- * @param {instance} dbConfig - Node-json-db configuration
- */
-const db = new JsonDB(dbConfig);
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
